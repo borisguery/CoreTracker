07c23edeeb3e95320a196cc0f947334465bd966b
Boris Guéry <guery.b@gmail.com>
2013-03-11 09:30:55 +0100

    updated test case, to test both filter option (inverse filter)

--- tests/Bgy/CoreTracker/Filter/CallThresholdStrategyTest.php
+++ tests/Bgy/CoreTracker/Filter/CallThresholdStrategyTest.php
@@ -2,11 +2,16 @@
 
 namespace Bgy\CoreTracker\Filter;
 
+use Bgy\CoreTracker\CollectedClass;
+
 class CallThresholdFilterStrategyTest extends \PHPUnit_Framework_TestCase
 {
     public function testShouldBeFiltered()
     {
         $f = new CallThresholdFilterStrategy(10);
-         $f->shouldBeFiltered(array('calls' => 5));
+        $c = new CollectedClass('Foo');
+        $c->calls = 5;
+        $this->assertTrue($f->shouldBeFiltered($c));
+        $this->assertFalse($f->shouldBeFiltered($c, true));
     }
 }
